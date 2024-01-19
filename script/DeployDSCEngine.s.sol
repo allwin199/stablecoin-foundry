d7ab3ec797220f175763715c61ad758436b2dbbe
worksofallwin <princeallwin199@gmail.com>
2024-01-19 16:26:32 +0530

    complete DSCEngine

--- script/DeployDSCEngine.s.sol
+++ script/DeployDSCEngine.s.sol
@@ -2,14 +2,32 @@
 pragma solidity 0.8.20;
 
 import {Script} from "forge-std/Script.sol";
+import {HelperConfig} from "./HelperConfig.s.sol";
 import {DSCEngine} from "../src/DSCEngine.sol";
 import {DecentralizedStableCoin} from "../src/DecentralizedStableCoin.sol";
 
 contract DeployDSCEngine is Script {
-    function run() external {
-        vm.startBroadcast();
+    address[] tokenAddresses;
+    address[] priceFeedAddresses;
+
+    function run() external returns (DSCEngine) {
         DecentralizedStableCoin dsCoin = new DecentralizedStableCoin();
-        DSCEngine dscEngine = new DSCEngine();
+        HelperConfig helperConfig = new HelperConfig();
+
+        (address weth, address wbtc, address wethUsdPriceFeed, address wbtcUsdPriceFeed, address deployerKey) =
+            helperConfig.activeNetworkConfig();
+
+        tokenAddresses.push(weth);
+        tokenAddresses.push(wbtc);
+
+        priceFeedAddresses.push(wethUsdPriceFeed);
+        priceFeedAddresses.push(wbtcUsdPriceFeed);
+
+        vm.startBroadcast(deployerKey);
+
+        DSCEngine dscEngine = new DSCEngine(tokenAddresses, priceFeedAddresses, address(dsCoin));
         vm.stopBroadcast();
+
+        return dscEngine;
     }
 }
